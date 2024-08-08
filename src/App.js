782a434a54606bb0bc89fbaca067678ce91da4e7
shailus1029 <shailus1029@gmail.com>
2024-08-08 22:18:21 +0530

    Adding lazy loading for routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import "./App.css";
-import Dashboard from "./Components/Dashboard.tsx";
+import React, { lazy, Suspense } from "react";
 import Home from "./Components/Home.tsx";
 import User from "./Components/User.tsx";
 import UserList from "./Components/Users.tsx";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./Components/Navbar.tsx";
-import NoMatch from "./Components/NoMatch.tsx"
-import ProductDetails from "./Components/ProductDetails.tsx";
+import NoMatch from "./Components/NoMatch.tsx";
+import "./App.css";
+
+/* Lazy loading routing */
+const Dashboard = lazy(() => import("./Components/Dashboard.tsx"));
+const ProductDetails = lazy(() => import("./Components/ProductDetails.tsx"));
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/usersList" element={<UserList />} />
-        <Route path="/userDetails" element={<User />} />
-        <Route path="/products/:slug" element={<ProductDetails />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+      <Suspense fallback={<div className="container">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/usersList" element={<UserList />} />
+          <Route path="/userDetails" element={<User />} />
+          <Route path="/products/:slug" element={<ProductDetails />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
